a35f867a1d32d69e9f41c1f59e5297ce8a00f759
Abhishek kumar <india52736628@gmail.com>
2025-01-02 11:02:13 +0530

    "cart working fine"

--- src/feature/cart/cart.js
+++ src/feature/cart/cart.js
@@ -9,10 +9,39 @@ const cartSlice = createSlice({
     initialState,
     reducers:{
         addState: (state, action) => {
-            state.cartState.push(action.payload)
-        },
+           
+            let id = action.payload.id;
+            let isPresent = state.cartState.find((item)=>item.id === id);
+            if(isPresent){
+
+                state.cartState.forEach((item) => {
+                    if(item.id === id){
+                        item.count++;
+                    }
+                    
+                })
+            }
+            else{
+                state.cartState.push({...action.payload, count:1})
+            }
+                },
+            
+           
+          
+       
+       
         removeState: (state, action) => {
-            state.cartState = state.cartState.filter((item) => item.id !== action.payload)
+            state.cartState.forEach((item) => {
+                if(item.id === action.payload){
+                    if(item.count > 1){
+                        item.count--;
+                    }
+                    else{
+                        state.cartState = state.cartState.filter((item) => item.id !== action.payload);
+                    }
+                }
+            })
+            // state.cartState = state.cartState.filter((item) => item?.id !== action.payload)
         }
     },
 
